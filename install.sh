9fe282f22f43b5d15ec02a4e84bdee7c6feff971
Felix Zimmermann <felix.zimmermann.1.de@gmail.com>
2023-11-06 17:36:29 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
-# manual: 
-# vscode intellij signal insomnia
+sudo apt install keepass2 xdotool git ca-certificates curl gnupg
 
-sudo apt install spotify
+sudo snap install code --classic
+sudo snap install spotify steam
 
 # docker
-https://docs.docker.com/engine/install/ubuntu/#install-using-the-repository
+https://docs.docker.com/engine/install/ubuntu/
+
+# signal
+https://www.signal.org/download/linux/
